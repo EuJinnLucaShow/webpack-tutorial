1d1f4af3262f794274a61bb0e6d971bda3c7f7a6
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-27 22:43:35 +0300

    Summary (required)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import "./styles/main.scss";
+
+import example from "./images/example.png";
+// Create a class property without a constructor
+class Game {
+  name = "Violin Charades";
+}
+const myGame = new Game();
+// Create paragraph node
+const p = document.createElement("p");
+p.textContent = `I like ${myGame.name}.`;
+
 // Create heading node
 const heading = document.createElement("h1");
 heading.textContent = "Interesting!";
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: "development",
   entry: {
     main: path.resolve(__dirname, "./src/index.js"),
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "main.bundle.js",
+    filename: "[name].bundle.js",
+  },
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+    // static: {
+    //   directory: path.resolve(__dirname, "./dist"),
+    // },
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -16,5 +28,33 @@ module.exports = {
       template: path.resolve(__dirname, "./src/template.html"), // template file
       filename: "index.html", // output file
     }),
+    new CleanWebpackPlugin(),
+    //  Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
 };
