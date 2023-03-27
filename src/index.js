44e3baac08bdde79795669dfb39d15e2908333fa
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-28 00:07:55 +0300

    Summary (required)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import "./styles/main.scss";
 
 import example from "./images/example.png";
+
+// Test a background image url in CSS
+const imageBackground = document.createElement("div");
+imageBackground.classList.add("image");
+
+const imagePublic = document.createElement("img");
+imagePublic.src = example;
+
 // Create a class property without a constructor
 class Game {
   name = "Violin Charades";
@@ -16,4 +24,4 @@ heading.textContent = "Interesting!";
 
 // Append heading node to the DOM
 const app = document.querySelector("#root");
-app.append(heading);
+app.append(heading, p, imagePublic);
